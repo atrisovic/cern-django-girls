bda67fc94274480eb21e8382de71ecc473d100f7
Ana Trisovic <ana.trisovic@gmail.com>
2017-06-15 10:25:18 +0200

    Added travis.

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from blog.models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+	user = User.objects.create_user(username='Ana', email='some@email.com', password='onion')
+	Post.objects.create(author=user, title="Test Case", text="This is a test.")
+	Post.objects.create(author=user, title="Another Test Case", text="This is another test.")
+    
+    def test_posts(self):
+	a = Post.objects.get(title = "Test Case")
+	t = Post.objects.get(title = "Another Test Case")
+	self.assertEqual(str(a), "Test Case")
+	self.assertEqual(str(t), "Another Test Case")
+
